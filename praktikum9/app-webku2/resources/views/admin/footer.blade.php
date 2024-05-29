c4be648b79a5d72204546bc6d779205d1e8f12f6
Marcrisca <146653561+Marcrisca@users.noreply.github.com>
2024-05-29 13:15:51 +0700

    Delete praktikum9/app-webku2 directory

--- praktikum9/app-webku2/resources/views/admin/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Praktikum 9</b> PW-2
-    </div>
-    <strong>Copyright &copy; 2024 <a href="https://adminlte.io">Risca Marcella Jhesica</a>.</strong> All rights reserved.
-  </footer>
